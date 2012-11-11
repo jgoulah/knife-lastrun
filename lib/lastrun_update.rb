ae6f2e3640f730bd435e97e93c6ab799cf87afd9
Brian Flad <bflad417@gmail.com>
2012-11-11 17:18:18 -0500

    Deprecation Fixes for Chef 11 Attribute Setting

--- lib/lastrun_update.rb
+++ lib/lastrun_update.rb
@@ -3,26 +3,26 @@ require 'chef/log'
 class LastRunUpdateHandler < Chef::Handler
 
   def report
-    node[:lastrun] = {}
+    node.set[:lastrun] = {}
 
-    node[:lastrun][:status] = run_status.success? ? "success" : "failed"
+    node.set[:lastrun][:status] = run_status.success? ? "success" : "failed"
 
-    node[:lastrun][:runtimes] = {}
-    node[:lastrun][:runtimes][:elapsed] = run_status.elapsed_time
-    node[:lastrun][:runtimes][:start]   = run_status.start_time
-    node[:lastrun][:runtimes][:end]     = run_status.end_time
+    node.set[:lastrun][:runtimes] = {}
+    node.set[:lastrun][:runtimes][:elapsed] = run_status.elapsed_time
+    node.set[:lastrun][:runtimes][:start]   = run_status.start_time
+    node.set[:lastrun][:runtimes][:end]     = run_status.end_time
 
-    node[:lastrun][:debug] = {}
-    node[:lastrun][:debug][:backtrace]           = run_status.backtrace
-    node[:lastrun][:debug][:exception]           = run_status.exception
-    node[:lastrun][:debug][:formatted_exception] = run_status.formatted_exception
+    node.set[:lastrun][:debug] = {}
+    node.set[:lastrun][:debug][:backtrace]           = run_status.backtrace
+    node.set[:lastrun][:debug][:exception]           = run_status.exception
+    node.set[:lastrun][:debug][:formatted_exception] = run_status.formatted_exception
 
-    node[:lastrun][:updated_resources] = []
+    node.set[:lastrun][:updated_resources] = []
     run_status.updated_resources.each do |resource|
       m = "recipe[#{resource.cookbook_name}::#{resource.recipe_name}] ran '#{resource.action}' on #{resource.resource_name} '#{resource.name}'"
       Chef::Log.debug(m)
 
-      node[:lastrun][:updated_resources].insert(0, {
+      node.set[:lastrun][:updated_resources].insert(0, {
         :cookbook_name => resource.cookbook_name,
         :recipe_name   => resource.recipe_name,
         :action        => resource.action,
