e8b301feba1293d1fdad97e04833c12b577ae6a5
Denis C <deniskin82@users.noreply.github.com>
2017-07-05 14:40:33 -0400

    Fix for chef13+

--- lib/lastrun_update.rb
+++ lib/lastrun_update.rb
@@ -4,26 +4,26 @@ class LastRunUpdateHandler < Chef::Handler
 
   def report
     _node = Chef::Node.load(node.name)
-    _node.set[:lastrun] = {}
+    _node.default[:lastrun] = {}
 
-    _node.set[:lastrun][:status] = run_status.success? ? "success" : "failed"
+    _node.default[:lastrun][:status] = run_status.success? ? "success" : "failed"
 
-    _node.set[:lastrun][:runtimes] = {}
-    _node.set[:lastrun][:runtimes][:elapsed] = run_status.elapsed_time
-    _node.set[:lastrun][:runtimes][:start]   = run_status.start_time
-    _node.set[:lastrun][:runtimes][:end]     = run_status.end_time
+    _node.default[:lastrun][:runtimes] = {}
+    _node.default[:lastrun][:runtimes][:elapsed] = run_status.elapsed_time
+    _node.default[:lastrun][:runtimes][:start]   = run_status.start_time
+    _node.default[:lastrun][:runtimes][:end]     = run_status.end_time
 
-    _node.set[:lastrun][:debug] = {}
-    _node.set[:lastrun][:debug][:backtrace]           = run_status.backtrace
-    _node.set[:lastrun][:debug][:exception]           = run_status.exception
-    _node.set[:lastrun][:debug][:formatted_exception] = run_status.formatted_exception
+    _node.default[:lastrun][:debug] = {}
+    _node.default[:lastrun][:debug][:backtrace]           = run_status.backtrace
+    _node.default[:lastrun][:debug][:exception]           = run_status.exception
+    _node.default[:lastrun][:debug][:formatted_exception] = run_status.formatted_exception
 
-    _node.set[:lastrun][:updated_resources] = []
+    _node.default[:lastrun][:updated_resources] = []
     Array(run_status.updated_resources).each do |resource|
       m = "recipe[#{resource.cookbook_name}::#{resource.recipe_name}] ran '#{resource.action}' on #{resource.resource_name} '#{resource.name}'"
       Chef::Log.debug(m)
 
-      _node.set[:lastrun][:updated_resources].insert(0, {
+      _node.default[:lastrun][:updated_resources].insert(0, {
         :cookbook_name => resource.cookbook_name,
         :recipe_name   => resource.recipe_name,
         :action        => resource.action,
